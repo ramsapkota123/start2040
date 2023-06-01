da8487a96e19c4d2e3caf59f4c9a9afebc7cc480
ramsapkota123 <rbsapkota99@gmail.com>
2023-06-01 10:27:53 -0500

    hialll

--- src/test/java/start2030/Actionss.java
+++ src/test/java/start2030/Actionss.java
@@ -3,6 +3,8 @@ package start2030;
 public class Actionss {
 	public static void main(String[]args) {
 		System.out.println("hello");
+		System.out.println("helloall");
+		System.out.println("hi");
 	}
 
 }
